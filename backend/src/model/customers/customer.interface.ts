2ff205e270e50ebb93934eb7108fe5d9ea36bf3f
Игорь Хорев <Igor.Horev@techincity.ru>
2022-09-06 15:10:02 +0300

    remove unused code

--- backend/src/model/customers/customer.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface Customer {
-    readonly id: number;
-    readonly firstName: string;
-    readonly secondName: string;
-    readonly lastName?: string;
-    readonly email?: string;
-    readonly phone?: string;
-    readonly birthday?: Date;
-}
