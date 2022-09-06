2db74d1d0a0c3bbd2db0bb3d54edd6dcb1b3e32b
Игорь Хорев <Igor.Horev@techincity.ru>
2022-09-06 15:09:19 +0300

    remove unused modules

--- backend/src/model/customers/customers.entity.spec.ts
+++
@@ -1,7 +0,0 @@
-import { CustomersEntity } from './customers.entity';
-
-describe('CustomersEntity', () => {
-  it('should be defined', () => {
-    expect(new CustomersEntity()).toBeDefined();
-  });
-});
